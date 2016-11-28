e5008387ad56f9e333af1231dbc391d10147fe68
Jeff Federman <jeff.federman@namely.com>
2016-11-28 11:08:02 -0500

    Remove vendored gem; Rubocop -> RuboCop

--- Support/lib/mate/formatter/base.rb
+++ Support/lib/mate/formatter/base.rb
@@ -7,12 +7,12 @@ module Mate
   module Formatter
     # This is used to format the response from rubocop
     # This is heavily influenced by the json formatter
-    class Base < Rubocop::Formatter::BaseFormatter
+    class Base < RuboCop::Formatter::BaseFormatter
       include ERB::Util
       include TemplateHelpers
       attr_reader :files, :summary, :files
 
-      def initialize(output)
+      def initialize(output, options={})
         super
         @files = []
         @summary = { offence_count: 0 }
@@ -34,7 +34,7 @@ module Mate
 
       def metadata
         {
-          :'RuboCop version' => Rubocop::Version::STRING,
+          :'RuboCop version' => RuboCop::Version::STRING,
           :'Ruby engine'     => RUBY_ENGINE,
           :'Ruby version'    => RUBY_VERSION,
           :'Ruby patchlevel' => RUBY_PATCHLEVEL.to_s,
